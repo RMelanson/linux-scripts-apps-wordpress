9563b613c659e3398ba32c488b7bcab20729d14c
Robin Melanson <remcogold@gmail.com>
2019-03-14 11:19:42 -0400

    Update WordpressBootstrap.sh

--- WordpressBootstrap.sh
+++ WordpressBootstrap.sh
@@ -12,9 +12,9 @@ fi
 #Git initialization installation
 yum install git -y
 
-#Set Cloning Properties
-#Need to setup to install wordpress under scripts/apps/
-pkg=wordpress
+# SETUP ENVIRONMENT AND PARAMETERS
+. ./env/setEnv.sh
+
 gitRepo="linux-scripts-apps-wordpress.git"
 installDir="/tmp/apps/wordpress"
 if [ -f ~/.ssh/gitHub.key ]; then
