e218f5bc22b799705c8f89dee94d00574cce3abb
Robin Melanson <robin.e.melanson@gmail.com>
2019-09-29 00:29:35 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -6,6 +6,7 @@ mySqlGroup=mySql
 mySqlHome=/opt/mySql
 mySqlLog=/var/log/mySql
 
+#Change this to see below
 #MySQL Params
 echo 'The dbName is: ' $1
 export dbName=$1
@@ -13,3 +14,11 @@ echo 'The userName is: ' $2
 export userName=$2
 echo 'The dbpwd is: ' $3
 export dbpwd=$3
+
+#Change to this
+echo Setting External Args
+echo These Arguments Overwrite Default Argument Settings
+for arg in "$@"; do
+  echo setArgs EXECUTING: export $arg
+  export $arg
+done
